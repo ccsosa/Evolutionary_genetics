0bb5da93754e81ff7107e332ba880b993b79ccf8
Chrystian <chrystiam.sossa@gmail.com>
2025-08-27 22:13:54 -0500

    no message

--- Semana3/Read_data_adegemet_SSR_solution.R
+++ Semana3/Read_data_adegemet_SSR_solution.R
@@ -21,30 +21,30 @@ tabAA <- adegenet::df2genind(tab[, mic],
                              NA.char = "0"
 )
 X <- adegenet::tab(tabAA, NA.method="mean")  # imputar NAs con la media
-
-pca1 <- dudi.pca(X, scale=FALSE, scannf=FALSE, nf=3)
-barplot(pca1$eig[1:50], main = "PCA eigenvalues", col = heat.colors(50))
-
-col <- funky(5)
-# scatter(pca1)
-
-s.class(pca1$li,as.factor(tab$Region),xax=1,yax=3, col=transp(col,.6), 
-        axesell=FALSE,
-        cstar=1, cpoint=0.5, grid=FALSE)
-add.scatter.eig(pca1$eig[1:20], 3,1,2)
-
-#SUMMARIES
-summary(tabAA)
-poppr(tabAA)
-#He
-adegenet::Hs(tabAA)
-#Ploidy
-adegenet::ploidy(tabAA)
-#HW
-pegas::hw.test(tabAA)
-#to pegas object
-pegAA <- pegas::genind2loci(tabAA)
-#Fst
-pegas::Fst(pegAA)
-pegas::haploNet(pegAA)
-pegas::H(pegAA)
+# 
+# pca1 <- dudi.pca(X, scale=FALSE, scannf=FALSE, nf=3)
+# barplot(pca1$eig[1:50], main = "PCA eigenvalues", col = heat.colors(50))
+# 
+# col <- funky(5)
+# # scatter(pca1)
+# 
+# s.class(pca1$li,as.factor(tab$Region),xax=1,yax=3, col=transp(col,.6), 
+#         axesell=FALSE,
+#         cstar=1, cpoint=0.5, grid=FALSE)
+# add.scatter.eig(pca1$eig[1:20], 3,1,2)
+# 
+# #SUMMARIES
+# summary(tabAA)
+# poppr(tabAA)
+# #He
+# adegenet::Hs(tabAA)
+# #Ploidy
+# adegenet::ploidy(tabAA)
+# #HW
+# pegas::hw.test(tabAA)
+# #to pegas object
+# pegAA <- pegas::genind2loci(tabAA)
+# #Fst
+# pegas::Fst(pegAA)
+# pegas::haploNet(pegAA)
+# pegas::H(pegAA)
